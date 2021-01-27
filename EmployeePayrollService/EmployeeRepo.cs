d99f45758cb9a48bdb21c054cefa68814420e110
abh224272 <abh224272@gmail.com>
2021-01-27 15:30:15 +0530

    [Abhishek Mishra] Add: Created connection between program to databse

---
+++ EmployeePayrollService/EmployeeRepo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeePayrollService
+{
+    class EmployeeRepo
+    {
+        public static string connectionString = @"Server=(Localdb)\\MSSQLLocalDB;database=Payroll_Service;Trusted_Connection=true";
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+        SqlCommand command;
+    }
+}
